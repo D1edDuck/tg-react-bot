1469fddf78e1f7c14a71e9185e499e4ce9071107
D1edDuck <mazurenkoi70@gmail.com>
2025-04-05 13:26:15 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  useEffect(() => {
+    tg.ready();
+  }, [])
+
+
+
   return (
     <div className="App">
-      work
+      <button onClick={onClose}>Закрыть</button>
     </div>
   );
 }
