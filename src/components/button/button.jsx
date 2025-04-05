3ff5a63bd593857980493036e9654793f9902887
D1edDuck <mazurenkoi70@gmail.com>
2025-04-05 19:09:11 +0300

    init

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -3,7 +3,7 @@
 
     const Button = (props) => {
         return (
-            <button {...props} className={'button' + props.className} />
+            <button {...props} className={'button ' + props.className} />
         )
     }
 
